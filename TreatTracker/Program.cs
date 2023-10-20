263a6bcfc6c4b33fa2e9680707d0d72108ec49e7
Jake Elsberry <jelsberry96@gmail.com>
2023-10-20 14:27:12 -0700

    change password requirements

--- TreatTracker/Program.cs
+++ TreatTracker/Program.cs
@@ -29,12 +29,12 @@ namespace Treats
             builder.Services.Configure<IdentityOptions>(options =>
             {
                 // Default Password settings.
-                options.Password.RequireDigit = true;
-                options.Password.RequireLowercase = true;
-                options.Password.RequireNonAlphanumeric = true;
-                options.Password.RequireUppercase = true;
-                options.Password.RequiredLength = 6;
-                options.Password.RequiredUniqueChars = 1;
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 1;
+                options.Password.RequiredUniqueChars = 0;
             });
 
             WebApplication app = builder.Build();
