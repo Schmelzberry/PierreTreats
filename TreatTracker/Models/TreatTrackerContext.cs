fb8288d7b12a7d4f8c47fa9b4fcf6b07f3f92a2d
Jake Elsberry <jelsberry96@gmail.com>
2023-10-20 10:15:29 -0700

    migration issue - WIP

--- TreatTracker/Models/TreatTrackerContext.cs
+++ TreatTracker/Models/TreatTrackerContext.cs
@@ -3,7 +3,8 @@ using Microsoft.EntityFrameworkCore;
 namespace TreatTracker.Models
 {
     public class TreatTrackerContext : DbContext
-    {   
+    {   public DbSet<Treat> Treats { get; set; }
+        public DbSet<Flavor> Flavors { get; set; }
         public TreatTrackerContext(DbContextOptions options)
         
             : base(options) { }
