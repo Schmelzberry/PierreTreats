39aad8ceb4bab97dafd05b7e0ffdbcd40ae33a16
Jake Elsberry <jelsberry96@gmail.com>
2023-10-22 11:29:21 -0700

    splash pages lists all treats and flavors

--- TreatTracker/Controllers/HomeController.cs
+++ TreatTracker/Controllers/HomeController.cs
@@ -2,15 +2,27 @@
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using TreatTracker.Models;
 
 namespace TreatTracker.Controllers
 {
   public class HomeController : Controller
   { 
+    private readonly TreatTrackerContext _db;
+
+    public HomeController(TreatTrackerContext db)
+    {
+      _db = db;
+    }
     [HttpGet("/")]
     public ActionResult Index()
     {
-      return View();
+      Treat[] treats = _db.Treats.ToArray();
+      Flavor[] flavors = _db.Flavors.ToArray();
+      Dictionary<string,object[]> model = new Dictionary<string, object[]>();
+      model.Add("treats", treats);
+      model.Add("flavors", flavors);
+      return View(model);
     }
   }
 }
